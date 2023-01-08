f9ad4d4b493750268e56917ac2764cba27f7f506
Aleksei Vaskov <prizrak1143@gmail.com>
2023-01-09 01:19:09 +0300

    Сохранение домашних работ 3

--- Home_Work_2/A_Task_Dop_Work/Program.cs
+++ Home_Work_2/A_Task_Dop_Work/Program.cs
@@ -5,9 +5,11 @@ int NumberOfProgrammers = Convert.ToInt32(Console.ReadLine());
 // string NumberOfProgrammers = Console.ReadLine();
 // int Number = NumberOfProgrammers.Substring (NumberOfProgrammers.Length, 1);
 
-
-
-if (NumberOfProgrammers <15 & NumberOfProgrammers >10 ) 
+if (NumberOfProgrammers <0)
+{
+     Console.WriteLine("Некорректное число!!! Нельзя вводить отрицательное число!!!");
+}
+else if (NumberOfProgrammers <15 & NumberOfProgrammers >10 ) 
 {
     Console.WriteLine(NumberOfProgrammers + " программистов");
 }
