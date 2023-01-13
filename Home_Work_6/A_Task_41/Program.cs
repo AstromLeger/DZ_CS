afd8e44c9ab0b7c7cae2f1868eeb573e234964d0
Aleksei Vaskov <prizrak1143@gmail.com>
2023-01-14 00:15:55 +0300

    домашняя работа 6

---
+++ Home_Work_6/A_Task_41/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задание 41
+
+Console.WriteLine("Сколько чисел вы хотите ввести?");
+int AmOfNum = Convert.ToInt32(Console.ReadLine());
+int[] Massiv = new int[AmOfNum];
+int PolNum = 0;
+
+for (int i = 0; i < AmOfNum; i++)
+{
+    Console.WriteLine("Введите пожалуйста число " + (i+1));
+    Massiv[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+// var str = string.Join(" ", Massiv);
+//     Console.WriteLine(str);
+
+int ColPolNum()
+{
+    for (int i = 0; i < AmOfNum; i++)
+    {
+        if (Massiv[i]>0)
+        {
+            PolNum = PolNum + 1;
+        }
+    }
+    return PolNum;
+}
+
+Console.WriteLine(ColPolNum());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
